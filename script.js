d37c8e9fbbf4ab327b6805ab96caf96a64d4b370
JulioCastro240902 <102335182+JulioCastro240902@users.noreply.github.com>
2022-06-08 20:32:10 -0300

    Fisrt Exercise

---
+++ script.js
@@ -0,0 +1,11 @@
+let nome = "Julio";
+let idade = 20;
+let ano = 2022;
+let subtracao = 2022 - idade;
+console.log(idade + ano);
+
+console.log("Estou mexendo no console.log");
+
+let quartaFeira = true;
+console.log(quartaFeira);
+console.log(typeof quartaFeira);
